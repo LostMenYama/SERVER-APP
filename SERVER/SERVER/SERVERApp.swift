0bd7dc0e387e38ad2c752bf68f9cbc21fb2ed9ff
daffashiddiq <daffashiddiq@gmail.com>
2024-09-22 21:40:03 +0700

    first commit

---
+++ SERVER/SERVER/SERVERApp.swift
@@ -0,0 +1,17 @@
+//
+//  SERVERApp.swift
+//  SERVER
+//
+//  Created by Daffashiddiq on 22/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SERVERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
